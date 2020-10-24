3e1aa0dad8e68a4d95b0ba893e665ef1e772ce78
MiguDeveloper <miguel.peru.seo@gmail.com>
2020-10-24 15:13:12 -0500

    ejercicios completos

--- src/transformacion/06-exhaustMap.ts
+++ src/transformacion/06-exhaustMap.ts
@@ -3,5 +3,5 @@ import { interval, fromEvent } from 'rxjs';
 
 const interval$ = interval(500).pipe(take(3));
 const click$ = fromEvent<MouseEvent>(document, 'click');
-
+// el exhaustMap solo mantiene una suscripcion activa para q emita valores
 click$.pipe(exhaustMap(() => interval$)).subscribe(console.log);
